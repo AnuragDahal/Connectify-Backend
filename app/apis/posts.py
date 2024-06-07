a9098f5b60164585156f1fdedf5a00b7e3360132
AnuragDahal <freefirepros2020@gmail.com>
2024-06-07 23:06:17 +0545

    feat: Add read and delete post endpoints

--- app/apis/posts.py
+++ app/apis/posts.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status
 from ..handlers.User.Posts.posthandler import PostsHandler
 from ..models import schemas
+from typing import List
 
 router = APIRouter(prefix='/api/v1/posts', tags=["Posts"])
 
@@ -11,3 +12,16 @@ async def create_post(request: schemas.Post):
     new_post = PostsHandler.HandlePostCreation(request)
     return new_post
 
+
+@router.get("/read", response_model=List[schemas.Post], status_code=status.HTTP_200_OK)
+async def get_posts():
+
+    posts = PostsHandler.HandlePostReadings()
+    return posts
+
+
+@router.delete("/delete/{post_id}", status_code=status.HTTP_200_OK)
+async def delete_post(post_id: str):
+
+    post = PostsHandler.HandlePostDeletion(post_id)
+    return post
--- app/handlers/User/Posts/posthandler.py
+++ app/handlers/User/Posts/posthandler.py
@@ -1,6 +1,7 @@
 
 from ....core.database import post_collection
 from ....models import schemas
+from ...exception import ErrorHandler
 
 
 class PostsHandler:
@@ -12,5 +13,28 @@ class PostsHandler:
         """
         new_post = post_collection.insert_one(
             {**request.model_dump(exclude=None)})
-        
+
         return {"id": str(new_post.inserted_id)}
+
+    @staticmethod
+    def HandlePostReadings():
+        """
+        Get all the posts.
+        """
+        count = post_collection.count_documents({})
+        if count > 0:
+            users = post_collection.find()
+            return users
+        else:
+            raise ErrorHandler.NotFound("No posts found")
+
+    @staticmethod
+    def HandlePostDeletion(post_id):
+        """
+        Delete a post.
+        """
+        post = post_collection.find_one_and_delete({"_id": post_id})
+        if post:
+            return {"message": "Post deleted for post id:"+str(post_id)}
+        else:
+            raise ErrorHandler.NotFound("Post not found")
