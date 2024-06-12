f847324a2825b80e31efc2b8d45c34eb02b2d2a8
AnuragDahal <freefirepros2020@gmail.com>
2024-06-12 22:15:27 +0545

    feat: Refactor post_id with bson Object_Id

--- app/handlers/User/Posts/posthandler.py
+++ app/handlers/User/Posts/posthandler.py
@@ -53,7 +53,7 @@ class PostsHandler:
         if post:
             # Delete all the comments for the post and then delete the post
             comments_collection.delete_many({"post_id": ObjectId(post_id)})
-            post_collection.delete_one({"post_id": ObjectId(post_id)})
+            post_collection.delete_one({"_id": ObjectId(post_id)})
             return {"message": "Post and comments deleted for post id:"+str(post_id)}
         else:
             raise ErrorHandler.NotFound("Post not found")
@@ -78,9 +78,9 @@ class PostsHandler:
         Update a post.
         """
         post = post_collection.find_one_and_update(
-            {"post_id": ObjectId(post_id)},
+            {"_id": ObjectId(post_id)},
             {"$set": {
-                **request.model_dump(exclude={"post_id"}), "post_id": ObjectId(post_id)}},
+                **request.model_dump(exclude=None), "_id": ObjectId(post_id)}},
             return_document=ReturnDocument.AFTER
         )
         if post:
@@ -94,7 +94,7 @@ class PostsHandler:
         """
         if not file:
             raise ErrorHandler.Error("No image found")
-        post = post_collection.find_one({"post_id": ObjectId(post_id)})
+        post = post_collection.find_one({"_id": ObjectId(post_id)})
         if post:
             img_id = file.filename.split(".")[0][:10]
             img_byte = file.file.read()
@@ -108,12 +108,12 @@ class PostsHandler:
         """
         Like a post.
         """
-        post = post_collection.find_one({"post_id": ObjectId(post_id)})
+        post = post_collection.find_one({"_id": ObjectId(post_id)})
         if post:
             if email in post["likes"]:
                 raise ErrorHandler.Error("Post already liked")
             post_collection.find_one_and_update(
-                {"post_id": ObjectId(post_id)},
+                {"_id": ObjectId(post_id)},
                 {"$addToSet": {"likes": email}}
             )
             return {"message": "Post liked"}
@@ -124,7 +124,7 @@ class PostsHandler:
         """
         Get the number of likes for a post.
         """
-        post = post_collection.find_one({"post_id": ObjectId(post_id)})
+        post = post_collection.find_one({"_id": ObjectId(post_id)})
         if post:
             count_likes = 0
             for like in post["likes"]:
@@ -150,10 +150,10 @@ class PostsHandler:
     @staticmethod
     def HandlePostPrivacyUpdate(post_id: str, privacy: str):
         """Change the privacy of your posts to public, friends or private."""
-        post = post_collection.find_one({"post_id": ObjectId(post_id)})
+        post = post_collection.find_one({"_id": ObjectId(post_id)})
         if post and privacy in ["public", "friends", "private"]:
             post_privacy = post_collection.find_one_and_update(
-                {"post_id": ObjectId(post_id)},
+                {"_id": ObjectId(post_id)},
                 {"$set": {"privacy": privacy}}, return_document=ReturnDocument.AFTER)
             return post_privacy
         return ErrorHandler.NotFound("Post not found")
