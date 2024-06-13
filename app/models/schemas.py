10cd4fe161944a78ba369db9fcd5db0282140b33
AnuragDahal <freefirepros2020@gmail.com>
2024-06-13 20:29:46 +0545

    feat: bug fix in post like

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel, Field
 from typing import List, Optional, Dict, Any
 from datetime import datetime, timezone
 import uuid
-from bson import ObjectId
 from enum import Enum
 
 
