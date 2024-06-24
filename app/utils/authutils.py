c64c5203b23f99444db26e665644893fd985425e
AnuragDahal <freefirepros2020@gmail.com>
2024-06-24 23:25:42 +0545

    chore: Add token revocation functionality

--- app/utils/authutils.py
+++ app/utils/authutils.py
@@ -4,6 +4,7 @@ from jose import jwt, JWTError
 from typing import Callable
 from functools import wraps
 from .envutils import Environment
+from app.core.database import token_collection
 env = Environment()
 
 env = Environment()
@@ -25,3 +26,16 @@ async def get_email_from_token(req: Request) -> str:
     else:
         raise HTTPException(
             status_code=401, detail="Could not find the appropriate headers")
+
+
+async def AddToRevocation(req: Request):
+
+    pass
+
+
+async def HandleRevocation(token: str) -> bool:
+
+    is_revocated = await token_collection.find_one({"token": token}).count_documents()
+    if is_revocated > 0:
+        return True
+    return False
