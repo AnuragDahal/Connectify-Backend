fb55e9101931cca0375c3228514088ea48be4063
AnuragDahal <freefirepros2020@gmail.com>
2024-06-30 22:25:38 +0545

    chore: Update CORS middleware to allow only http://localhost:5173 as origin

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="CONNECTIFY", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
